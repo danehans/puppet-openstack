7d0ba3119b378d5003dd40de827cc11c6c66ccef
danehans <danehans@cisco.com>
2012-12-19 16:14:37 +0000

    uncommented enable parameter for quantum

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -164,7 +164,7 @@ class openstack::compute(
 
   # Base Quantum Class to manage quantum.conf for Compute Nodes
   class { 'quantum':
-    #enabled                => false,
+    enabled                => false,
     rabbit_host            => $quantum_rabbit_host,
     rabbit_port            => '5672',
     rabbit_user            => $quantum_rabbit_user,
